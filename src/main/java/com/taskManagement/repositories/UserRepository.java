e2e29926e415b5b60da2a03482157fa19527b9d5
Vitor Andrade <vitor.andrade@corp.grupopneubras.com>
2023-06-22 20:32:46 -0300

    classe user, role, exceptions, dto e validation

---
+++ src/main/java/com/taskManagement/repositories/UserRepository.java
@@ -0,0 +1,18 @@
+package com.taskManagement.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.taskManagement.entities.User;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+	User findByEmail(String email);
+
+	@Query("from User where descruser =  ?1")
+	public List<User> buscarusuariologado(String descruser);
+	
+	
+}
