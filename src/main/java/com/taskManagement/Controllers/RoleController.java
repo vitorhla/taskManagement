209015b440d7d7d755b20f6263f0c6ee2db72ab5
Vitor Andrade <vitor.andrade@corp.grupopneubras.com>
2023-06-22 21:19:32 -0300

    Processo de login com springsecurity concluido

---
+++ src/main/java/com/taskManagement/Controllers/RoleController.java
@@ -0,0 +1,28 @@
+package com.taskManagement.Controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.taskManagement.entities.Role;
+import com.taskManagement.services.RoleService;
+
+@RestController
+@RequestMapping(value =  "/Regras")
+public class RoleController {
+	
+	@Autowired
+	private RoleService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Role>> findAll(){
+	List<Role> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	
+}
--- src/main/java/com/taskManagement/entities/User.java
+++ src/main/java/com/taskManagement/entities/User.java
@@ -34,7 +34,7 @@ public class User implements UserDetails,Serializable{
 	private String password;
 
 	@ManyToMany(fetch =  FetchType.EAGER)
-	@JoinTable(name="usarRole",
+	@JoinTable(name="userRole",
 	joinColumns = @JoinColumn(name="user_id"),
 	inverseJoinColumns = @JoinColumn(name="role_id"))
 	private Set<Role> roles =  new HashSet<>();
